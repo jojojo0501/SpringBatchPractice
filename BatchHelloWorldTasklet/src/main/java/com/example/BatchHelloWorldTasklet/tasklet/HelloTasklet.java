8b7c25c1c2f55c26f76e116a906e3387fa39c823
jojojo0501 <sarukiti0418@outlook.jp>
2022-08-13 22:26:08 +0900

    create tasklet sample project

--- BatchHelloWorldTasklet/src/main/java/com/example/BatchHelloWorldTasklet/tasklet/HelloTasklet.java
+++ BatchHelloWorldTasklet/src/main/java/com/example/BatchHelloWorldTasklet/tasklet/HelloTasklet.java
@@ -1,4 +1,22 @@
 package com.example.BatchHelloWorldTasklet.tasklet;
 
-public class HelloTasklet {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@StepScope
+@Slf4j
+public class HelloTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+        log.info("Hello World!!");
+        return RepeatStatus.FINISHED;
+    }
 }
